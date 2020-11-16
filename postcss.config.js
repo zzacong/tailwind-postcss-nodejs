bf7b43b2c3a57407d816ebdea4c667892c4ad526
zzacong <ongzz100@gmail.com>
2020-11-17 00:56:18 +1100

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' &&
+      purgecss({
+        content: ['./public/index.html'],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      }),
+  ],
+}
